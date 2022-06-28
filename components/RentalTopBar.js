82103e42deee857b4b9402aa83a5ed8b2a6a7eee
Hassan Aftab <87358302+hassannaftabb@users.noreply.github.com>
2022-06-28 14:10:38 -0700

    Update RentalTopBar.js

--- components/RentalTopBar.js
+++ components/RentalTopBar.js
@@ -24,7 +24,7 @@ const ManPowerTopBar = () => {
           <li>
             <a
               className="block transition-colors hover:text-gray-700"
-              href="/collections/shirts"
+              href="#"
             >
               Rental Services
             </a>
